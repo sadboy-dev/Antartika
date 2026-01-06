7047078b8cf99a666899f358a672c03baa9c4e1c
Jriik89 <110338305+sadboy-dev@users.noreply.github.com>
2026-01-05 21:30:30 -0800

    Create finish.lua

---
+++ finish.lua
@@ -0,0 +1,5 @@
+return {
+CFrame.new(-3718.99, 255.00, 235.67) * CFrame.Angles(0,0,0),
+CFrame.new(-3719.99, 255.00, 235.50) * CFrame.Angles(0,0,0),
+CFrame.new(-3720.61, 255.01, 236.69) * CFrame.Angles(0,0,0)
+}
