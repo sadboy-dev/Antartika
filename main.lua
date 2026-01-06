0962c5ed0800a2145b1469a5cf0acb437435a7d0
Jriik89 <110338305+sadboy-dev@users.noreply.github.com>
2026-01-06 18:21:52 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -236,7 +236,7 @@ cp3.MouseButton1Down:connect(function()
     local plr = game.Players.LocalPlayer
     local root = plr.Character and plr.Character:FindFirstChild("HumanoidRootPart")
     if root then
-        safeTeleport(root, CFrame.new(5876.31, 326.54, -220.64))
+        safeTeleport(root, CFrame.new(5962.76, 321.27, 41.44))
     end
  end)
 
